f774d98a64b2ab566daa233503a4ec4b8c718238
Peter MacNaughton <peter@macnaughton.info>
2014-05-01 22:56:11 -0600

    Added IEnumerable<T> extensions for great good

---
+++ Specific.Tests/IEnumerableExtensionTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace Specific.Tests
+{
+    public class HigherThan : ISpecification<int>
+    {
+        readonly int limit;
+
+        public HigherThan(int limit)
+        {
+            this.limit = limit;
+        }
+
+        public bool SatisfiedBy(int entity)
+        {
+            return entity > limit;
+        }
+    }
+
+    public class DivisibleBy : ISpecification<int>
+    {
+        readonly int divisor;
+
+        public DivisibleBy(int divisor)
+        {
+            this.divisor = divisor;
+        }
+
+        public bool SatisfiedBy(int entity)
+        {
+            return entity % divisor == 0;
+        }
+    }
+
+    public class IsSpecialNumber : AndSpecification<int>
+    {
+        public IsSpecialNumber()
+            : base(new HigherThan(80), new DivisibleBy(5))
+        {
+        }
+    }
+
+    public class IEnumerableExtensionTests
+    {
+        [Fact]
+        public void Meh()
+        {
+            var specialOnes = Enumerable.Range(1, 100).Where(new IsSpecialNumber());
+            specialOnes.Count().ShouldBe(4);
+            specialOnes.ShouldContain(85);
+            specialOnes.ShouldContain(95);
+            specialOnes.ShouldContain(95);
+            specialOnes.ShouldContain(100);
+        }
+    }
+}
---
+++ Specific/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specific
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> items, ISpecification<T> specification)
+        {
+            return items.Where(specification.SatisfiedBy);
+        }
+    }
+}
