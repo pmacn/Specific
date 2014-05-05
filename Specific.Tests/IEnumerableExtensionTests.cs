adf16a438e23540bc613ff59b4a897a81a2726e3
pmacnaughton <peter@macnaughton.info>
2014-05-05 14:35:29 -0600

    removed redundant using statements

--- Specific.Tests/IEnumerableExtensionTests.cs
+++ Specific.Tests/IEnumerableExtensionTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using System.Linq;
 using Shouldly;
+using Xunit;
 
 namespace Specific.Tests
 {
