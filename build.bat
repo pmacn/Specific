2330ad590dba3e1737a99f7e4d7b2fd8cecd8dd8
Peter MacNaughton <peter@macnaughton.info>
2015-04-09 23:17:03 -0600

    Update build script to use xUnit 2.0

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
-"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "xunit.runners" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\FAKE\tools\Fake.exe" build.fsx %1
+tools\nuget\nuget.exe install FAKE.Core -OutputDirectory tools -ExcludeVersion
+tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -ExcludeVersion
+tools\nuget\nuget.exe restore src\Specific.sln
+tools\FAKE.Core\tools\Fake.exe build.fsx %1
